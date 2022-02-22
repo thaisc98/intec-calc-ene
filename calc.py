e399bf739dfa09cb9d11889e7ee6e3f998257565
Jeffrey <slassarg20@gmail.com>
2022-02-22 15:29:12 -0400

    Feature resta added and tested

--- calc.py
+++ calc.py
@@ -3,3 +3,8 @@ class Calc:
         if num1 < 0 or num2 < 0:
             return "Invalid"
         return num1 + num2
+
+    def restar(self, num1, num2):
+        if num1 < 0 or num2 < 0:
+           return "Invalid"
+        return num1 - num2
