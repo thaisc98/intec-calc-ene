225686691e09dfd4f8c68d959fb775c7556e44f1
Miguel Jiménez <miguelp90.319@gmail.com>
2022-03-02 18:51:03 -0400

    [FIX] Square return value

--- test_calc.py
+++ test_calc.py
@@ -12,6 +12,14 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(0, self.calc.sumar(0, 0))
         self.assertEqual("Invalid", self.calc.sumar(1, -1))
         
+
+    def test_multiplicar(self):
+        self.assertEqual(6, self.calc.multiplicar(2, 3))
+        self.assertEqual(12, self.calc.multiplicar(3, 4))
+        self.assertEqual(100, self.calc.multiplicar(50, 2))
+        self.assertEqual(0, self.calc.multiplicar(5, 0))
+        self.assertEqual("Invalid", self.calc.multiplicar(3, -2))
+        
     def test_resta(self):
         self.assertEqual(0,self.calc.restar(2, 2))
         self.assertEqual(10,self.calc.restar(20, 10))
